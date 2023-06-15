485fdead46f0cea1fdac10373356a12190846e42
MOHAMED <mohamedelafandy593@gmail.com>
2023-06-15 14:06:43 -0700

    m

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: A C program that prints with put function
+ *
+ *Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	ptintf("Size of a char: %d byte(s)".sizeof(char));
+	ptintf("Size of an int: %d byte(s)".sizeof(int);
+	ptintf("Size of a long int: %d byte(s)".sizeof(long int);
+	ptintf("Size of a long long int: %d byte(s)".sizeof(long long int);
+	ptintf("Size of a float: %d byte(s)".sizeof(float);
+	return (0);
+}
+
