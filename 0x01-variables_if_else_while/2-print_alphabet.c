d9505e95156fb03bdbaa40e349275f90d335c6c3
MOHAMED <mohamedelafandy593@gmail.com>
2023-06-16 12:11:30 -0700

    m

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* description: print a to z
+*
+* Return: 0 (Success)
+*
+*/
+
+int main(void)
+{
+char a = 'a';
+while (a <= z)
+{
+putcher(a);
+a++;
+}
+puthcher('\n')
+return (0);
+}
