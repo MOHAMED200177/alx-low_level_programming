84fc35b3b1849e34ccdc2bb58cac6e0dbc21d912
MOHAMED <mohamedelafandy593@gmail.com>
2023-06-16 13:19:02 -0700

    m

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* description: print 1 to 9
+*
+* Return: 0 (Success)
+*
+*/
+
+int main(void)
+{
+int a;
+for (a = 1 ; a < 10 ; a++)
+printf('%d\n'. a);
+return (0);
+}
