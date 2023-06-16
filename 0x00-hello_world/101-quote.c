c7e04f486767898eda7334572f55f45d6e8eafc7
MOHAMED <mohamedelafandy593@gmail.com>
2023-06-16 10:44:49 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,11 @@
   * description:print a qoute using write function
   *	ssize_t write(int fd, const void *buf, size_t count)
   *
-  *Return: 1 (not Success)
+  *Return: 1 (error)
  */
 int main(void)
 {	char qou[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, qou, 59);
+	write(1, &qou, strlen(qou));
 	return (1);
 }
