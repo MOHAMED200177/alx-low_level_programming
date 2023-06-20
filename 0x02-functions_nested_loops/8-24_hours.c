581ec6570bd4ee991d764c16e5990ba3d27ea847
MOHAMED <mohamedelafandy593@gmail.com>
2023-06-20 16:43:07 -0700

    m

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,10 +4,12 @@
 * jack_bauer -  prints every minute of the day
 */
 void jack_bauer(void)
+int h;
+int m;
 {
-for (int h = 0; h <= 23; h++)
+for (h = 0; h <= 23; h++)
 {
-for (int m = 0; m <= 59; m++)
+for (m = 0; m <= 59; m++)
 {
 _putchar ((h / 10) + 48);
 _putchar ((h / 10) + 48);
