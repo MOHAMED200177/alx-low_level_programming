d672919cc929e00b65ee6936246fd4895b9b6e8a
MOHAMED <mohamedelafandy593@gmail.com>
2023-06-22 13:05:07 -0700

    m

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* more_numbers -   draws a straight line in the terminal.
+* @n : input
+* Return: always 0 (success)
+*/
+
+void print_line(int n)
+{
+int n = $
+__$
+__________$
+$;
+_putchar (n);
+}
