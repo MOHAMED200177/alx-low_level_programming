0637316f8042259c88adb28f87c7b206ba031289
MOHAMED <mohamedelafandy593@gmail.com>
2023-07-03 13:19:08 -0700

    m

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,7 +7,8 @@
  *@needle: input
  * Return: Always 0.
  */
-char *_strstr(char *haystack, char *needle) {
+char *_strstr(char *haystack, char *needle)
+{
 while (*haystack != '\0')
 {
 char *h = haystack;
