f14a3cfff6f9c870f72d230d6e92badb78fb0da1
MOHAMED <mohamedelafandy593@gmail.com>
2023-07-03 11:02:00 -0700

    m

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,7 +8,7 @@
  *@n: input
  * Return: Always 0.
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
 for (i = 0; n > 0; i++, n--)
