c6d0855dc8f5194dd348c84fa04573d885ea77ec
MOHAMED <mohamedelafandy593@gmail.com>
2023-07-17 12:25:26 -0700

    m

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog - a dog struct
+*@name : dogs name
+*@age : dogs age
+*@owner : owner name
+* Return: always 0 (success)
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+
+#endif /* _OBJECT_LIKE_MACRO_H */
