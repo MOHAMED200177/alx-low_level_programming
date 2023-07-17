3d8bf209d86d4e59904477a0647b1cb639473f28
MOHAMED <mohamedelafandy593@gmail.com>
2023-07-17 13:18:59 -0700

    m

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -59,3 +59,21 @@ len++;
 
 return (len);
 }
+/**
+ * _strcpy - copies a string from src to dest
+ * @dest: destination buffer
+ * @src: source buffer
+ *
+ * Return: pointer to dest buffer
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+
+for (i = 0; src[i]; i++)
+dest[i] = src[i];
+
+dest[i] = '\0';
+
+return (dest)
+}
