dbb613497d9505ed29bbda0ed06697d091ae0399
MOHAMED <mohamedelafandy593@gmail.com>
2023-07-19 13:08:59 -0700

    m

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,24 +1,7 @@
+#include "function_pointers.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-
-/**
- * print_opcodes - prints the opcodes of its own main function
- * @n: input
- *
-* Return: 0 (success)
- */
-
-
-
-void print_opcodes(int n)
-{
-char *p = (char *)print_opcodes;
-for (int i = 0; i < n; i++)
-{
-printf("%02hhx ", p[i]);
-}
-printf("\n");
-}
 
 
 /**
@@ -31,17 +14,16 @@ printf("\n");
 
 int main(int argc, char **argv)
 {
+char *p = (char *)main;
+int b;
 if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
-int n = atoi(argv[1]);
-if (n < 0)
-{
-printf("Error\n");
-return (2);
-}
-print_opcodes(n);
+printf("Error\n"), exit(1);
+
+b = atoi(argv[1]);
+if (b < 0)
+printf("Error\n"), exit(2);
+
+while (b--)
+printf("%02hhx%s", *p++, b ? " " : "\n")
 return (0);
 }
