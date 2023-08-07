9d43348b4e97db857db2e5283cd85eae0cee8d0e
MOHAMED <mohamedelafandy593@gmail.com>
2023-08-07 13:09:24 -0700

    m

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,30 +6,37 @@
  *
  * Return: integer length of string
  */
+int _strlen(char *s)
+{
+int i = 0;
 
+if (!s)
+return (0);
+
+while (*s++)
+i++;
+return (i);
+}
+
+/**
+ * append_text_to_file - appends text to a file
+ * @filename: name of file to create
+ * @text_content: text to write
+ *
+ * Return: 1 on success 0 on failure
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 int fd;
-ssize_t len = 0;
+ssize_t bytes = 0, len = _strlen(text_content);
 
-if (filename == NULL)
+if (!filename)
 return (-1);
-
 fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
-
-if (text_content != NULL)
-{
-while (text_content[len])
-len++;
-
-if (write(fd, text_content, len) != len)
-return (-1);
-}
-
+if (len)
+bytes = write(fd, text_content, len);
 close(fd);
-
-return (1);
+return (bytes == len ? 1 : -1);
 }
-
