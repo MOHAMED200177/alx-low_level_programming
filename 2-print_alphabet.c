e482f70372055e7e5eefde660470ea52ed393cfd
MOHAMED <mohamedelafandy593@gmail.com>
2023-09-16 00:24:50 +0300

    'if'

---
+++ 2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* description: print a to z
+*
+* Return: 0 (Success)
+*
+*/
+
+int main(void)
+{
+char a = 'a';
+while (a <= 'z')
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
+}
